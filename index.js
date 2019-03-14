b68c45e4a97ed435b2214254fa0672f678c9f5b5
trymycode <imitasingha1994@gmail.com>
2019-03-14 15:00:28 +0530

    added few things

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-var express = require('express');
-var socket = require('socket.io');
-// App setup
+const express = require('express');
+const path = require('path');
 var app = express();
-var server = app.listen(4000, function(){
-  console.log('Server is listening to request on port 4000.');
-});
-// static files
-app.use(express.static('public'));
+const PORT = process.env.PORT || 5000;
+var socket = require('socket.io');
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+var server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+
 // socket setup
 var io = socket(server);
 io.on('connection', function(socket){
--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('http://localhost:5000');
 
 // query DOM
 var message = document.getElementById('message'),
